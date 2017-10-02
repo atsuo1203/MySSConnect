beabbe4832d965b64b7cf501803e354be1faa69e
atsuo1203 <dondonkitemasu@gmail.com>
2017-10-02 16:00:12 +0900

    レイアウト完成

--- MySSConnect/SearchViewController.swift
+++ MySSConnect/SearchViewController.swift
@@ -8,16 +8,22 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
+class SearchViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var searchImageView: UIImageView!
-
+    @IBOutlet weak var tagTextField: UITextField!
+    @IBOutlet weak var titleTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        //searchButton
         searchImageView.isUserInteractionEnabled = true
         let gesture = UITapGestureRecognizer(target: self, action: #selector(self.search))
         searchImageView.addGestureRecognizer(gesture)
+        //textField
+        self.tagTextField.delegate = self
+        self.titleTextField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,12 +31,27 @@ class SearchViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if (self.tagTextField.isFirstResponder) {
+            self.tagTextField.resignFirstResponder()
+        }
+        
+        if (self.titleTextField.isFirstResponder) {
+            self.titleTextField.resignFirstResponder()
+        }
+    }
+    
     func search() {
         let storyboard = UIStoryboard(name: "Result", bundle: nil)
         guard let nextVC = storyboard.instantiateInitialViewController() else {return}
         self.navigationController?.pushViewController(nextVC, animated: true)
     }
-
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
     /*
     // MARK: - Navigation
 
