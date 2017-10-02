302d36820d83af4191265e3a86ebf0d82f0438da
atsuo1203 <dondonkitemasu@gmail.com>
2017-10-02 16:14:53 +0900

    XIB作成

---
+++ MySSConnect/MainTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  MainTableViewCell.swift
+//  MySSConnect
+//
+//  Created by Atsuo Yonehara on 2017/10/02.
+//  Copyright © 2017年 Atsuo Yonehara. All rights reserved.
+//
+
+import UIKit
+
+class MainTableViewCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var blogPickerView: UIPickerView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
