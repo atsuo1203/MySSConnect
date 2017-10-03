dce54bac737c6e2a04e9d7b741589b1590938604
atsuo1203 <dondonkitemasu@gmail.com>
2017-10-03 18:19:11 +0900

    表記調整

--- MySSConnect/Article.swift
+++ MySSConnect/Article.swift
@@ -10,13 +10,13 @@ import UIKit
 import SwiftyJSON
 
 class Article: NSObject {
-    var id = ""
+    var id = 0
     var url = ""
     var posted_at = ""
     var blog: Blog?
     
     init(json: JSON) {
-        self.id = json["id"].description
+        self.id = json["id"].intValue
         self.url = json["url"].description
         self.posted_at = json["posted_at"].description
         self.blog = Blog(json: json)
--- MySSConnect/Blog.swift
+++ MySSConnect/Blog.swift
@@ -10,14 +10,14 @@ import UIKit
 import SwiftyJSON
 
 class Blog: NSObject {
-    var id: String?
-    var title: String?
-    var rss: String?
-    var created_at: String?
-    var updated_at: String?
+    var id = 0
+    var title = ""
+    var rss = ""
+    var created_at = ""
+    var updated_at = ""
     
     init(json: JSON) {
-        self.id = json["id"].description
+        self.id = json["id"].intValue
         self.title = json["title"].description
         self.rss = json["rss"].description
         self.created_at = json["created_at"].description
