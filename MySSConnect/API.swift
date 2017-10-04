32b7cda65e411610f602cdbc4de7012c110686fd
atsuo1203 <dondonkitemasu@gmail.com>
2017-10-04 18:14:21 +0900

    API拡張

--- MySSConnect/API.swift
+++ MySSConnect/API.swift
@@ -20,6 +20,13 @@ class API {
         return response
     }
     
+    static func getRequest(tag: String, q: String, page: String) -> DataRequest {
+        let url = baseURL+"tag="+tag+"q="+q+"page="+page
+        let response = Alamofire.request(url, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: nil)
+        print(response)
+        return response
+    }
+    
     static func postRequest(name: String, description: String){
         let parameters: Parameters = [
             "name": name,
