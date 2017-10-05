689a4b30a577b60387db7c954b377d49afff0a6d
atsuo1203 <dondonkitemasu@gmail.com>
2017-10-06 02:36:11 +0900

    Result作成

--- MySSConnect/ResultViewController.swift
+++ MySSConnect/ResultViewController.swift
@@ -7,29 +7,75 @@
 //
 
 import UIKit
+import SwiftyJSON
 
 class ResultViewController: UIViewController {
-
+    @IBOutlet weak var mainTableView: UITableView!
+    //受け取るやつ
+    var stories: [Story]?
+    var tag: String?
+    //page
+    var page = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.mainTableView.delegate = self
+        self.mainTableView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func getStories(){
+        self.mainTableView.reloadData()
+        
+        API.getStories(tag: tag!, q: "", page: page.description).responseJSON { (response) in
+            guard let object = response.result.value else {
+                return
+            }
+            let json = JSON(object)
+            json.forEach { (_, json) in
+                let story = Story(json: json)
+                self.stories!.append(story)
+            }
+            self.mainTableView.reloadData()
+        }
     }
-    */
+    
+}
 
+extension ResultViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return stories!.count + 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.row < stories!.count {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "MainCell", for: indexPath) as! MainTableViewCell
+            cell.selectionStyle = .none
+            cell.titleLabel?.text = stories![indexPath.row].title
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "AddCell", for: indexPath) as! AddTableViewCell
+            cell.selectionStyle = .none
+            return cell
+        }
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row < stories!.count {
+            //押されたcellを取得
+            let cell = tableView.cellForRow(at: indexPath) as! MainTableViewCell
+            //何番目が押されたかを取得
+            guard let row = self.mainTableView.indexPath(for: cell)?.row else {
+                return
+            }
+            API.showWebView(viewController: self, targetURL: stories![row].articles[0].url)
+        } else {
+            self.page += 1
+            getStories()
+        }
+    }
 }
