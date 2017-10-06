eec616cd53aba4ae1891222a1aec7fdd60279e4e
atsuo1203 <dondonkitemasu@gmail.com>
2017-10-07 00:06:46 +0900

    checkButtonつけた

--- MySSConnect/SettingViewController.swift
+++ MySSConnect/SettingViewController.swift
@@ -9,27 +9,20 @@
 import UIKit
 
 class SettingViewController: UIViewController {
+    @IBOutlet weak var checkImageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        checkImageView.isUserInteractionEnabled = true
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(self.checkButtonTapped))
+        checkImageView.addGestureRecognizer(gesture)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func checkButtonTapped(){
+        print("check is tapped")
     }
-    */
-
 }
