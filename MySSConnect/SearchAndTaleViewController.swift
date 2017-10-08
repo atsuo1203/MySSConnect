1d44404f4f777b3d4c6abaa91c8dcafda45aa051
atsuo1203 <dondonkitemasu@gmail.com>
2017-10-09 04:05:55 +0900

    Switch

--- MySSConnect/SearchAndTaleViewController.swift
+++ MySSConnect/SearchAndTaleViewController.swift
@@ -9,27 +9,51 @@
 import UIKit
 import SwiftyJSON
 
+enum MyController {
+    case tag
+    case favorite
+    init() {
+        self = .tag
+    }
+}
+
 class SearchAndTableViewController: UIViewController {
     @IBOutlet weak var mainSearchBar: UISearchBar!
     @IBOutlet weak var mainTableView: UITableView!
     var tags = [Tag]()
-    var searchResult = [Tag]()
-
+    var tagsResult = [Tag]()
+    var stories = [RealmStory]()
+    var storiesResult = [RealmStory]()
+    var type = MyController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         //tableView
         self.mainTableView.delegate = self
         self.mainTableView.dataSource = self
-        self.mainTableView.register(UINib(nibName: "TagCell", bundle: nil), forCellReuseIdentifier: "TagCell")
         //searchBar
         self.mainSearchBar.delegate = self
         self.mainSearchBar.enablesReturnKeyAutomatically = false
         //tag取得
-        getTags()
+        //tagBarかfavoriteBarかで処理を分離
+        if self.title! == "Tag" {
+            type = .tag
+            self.mainTableView.register(UINib(nibName: "TagCell", bundle: nil), forCellReuseIdentifier: "TagCell")
+            getTags()
+        }
+        if self.title! == "Favorite" {
+            type = .favorite
+            self.mainTableView.register(UINib(nibName: "MainCell", bundle: nil), forCellReuseIdentifier: "MainCell")
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
     }
     
     func getTags(){
@@ -44,7 +68,7 @@ class SearchAndTableViewController: UIViewController {
                     self.tags.append(tag)
                 }
             }
-            self.searchResult = self.tags
+            self.tagsResult = self.tags
             self.mainTableView.reloadData()
         }
     }
@@ -52,21 +76,26 @@ class SearchAndTableViewController: UIViewController {
 
 extension SearchAndTableViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return searchResult.count
+        switch type {
+        case .tag:
+            return tagsResult.count
+        case .favorite:
+            return storiesResult.count
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TagCell", for: indexPath) as! TagTableViewCell
         cell.selectionStyle = .none
-        cell.nameLabel.text = searchResult[indexPath.row].name
-        cell.countLabel.text = "(" + searchResult[indexPath.row].taggings_count.description + ")"
+        cell.nameLabel.text = tagsResult[indexPath.row].name
+        cell.countLabel.text = "(" + tagsResult[indexPath.row].taggings_count.description + ")"
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let storyboard = UIStoryboard(name: "Result", bundle: nil)
         let nextVC = storyboard.instantiateInitialViewController() as! ResultViewController
-        nextVC.tag = searchResult[indexPath.row].name
+        nextVC.tag = tagsResult[indexPath.row].name
         self.navigationController?.pushViewController(nextVC, animated: true)
     }
 }
@@ -75,14 +104,14 @@ extension SearchAndTableViewController: UISearchBarDelegate {
     //searchBarで検索した時の処理
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         mainSearchBar.endEditing(true)
-        searchResult.removeAll()
+        tagsResult.removeAll()
         
         if(mainSearchBar.text == "") {
-            searchResult = tags
+            tagsResult = tags
         } else {
             for tag in tags {
                 if tag.name.contains(mainSearchBar.text!) {
-                    searchResult.append(tag)
+                    tagsResult.append(tag)
                 }
             }
         }
